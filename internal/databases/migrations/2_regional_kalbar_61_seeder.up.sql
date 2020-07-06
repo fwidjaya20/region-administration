c5cd6651eaf68afe577246cca6f008f4f0048e12
Fredrick Widjaya <f.widjaya20@gmail.com>
2020-07-06 13:37:43 +0700

    WIP Kalbar Regional Seeder

---
+++ internal/databases/migrations/2_regional_kalbar_61_seeder.up.sql
@@ -0,0 +1,29 @@
+INSERT INTO public."provinces" ("code", "name", "created_by") VALUES
+('61', 'KALIMANTAN BARAT', 'SEEDER');
+
+INSERT INTO public."regenecies" ("code", "name", "created_by") VALUES
+('61.01', 'KAB. SAMBAS', 'SEEDER');
+
+INSERT INTO public."districts" ("code", "name", "created_by") VALUES
+('61.01.01', 'Sambas', 'SEEDER'),
+('61.01.02', 'Teluk Keramat', 'SEEDER');
+
+INSERT INTO public."villages" ("code", "name", "postal", "created_by") VALUES
+('61.01.01.2001', 'Dalam Kaum', '79460', 'SEEDER'),
+('61.01.01.2002', 'Lubuk Dagang', '79460', 'SEEDER'),
+('61.01.01.2003', 'Tanjung Bugis', '79460', 'SEEDER'),
+('61.01.01.2004', 'Pendawan', '79460', 'SEEDER'),
+('61.01.01.2005', 'Pasar Melayu', '79460', 'SEEDER'),
+('61.01.01.2006', 'Durian', '79460', 'SEEDER'),
+('61.01.01.2007', 'Lorong', '79460', 'SEEDER'),
+('61.01.01.2008', 'Jagur', '79460', 'SEEDER'),
+('61.01.01.2009', 'Tumuk Manggis', '79460', 'SEEDER'),
+('61.01.01.2010', 'Tanjung Mekar', '79460', 'SEEDER'),
+('61.01.01.2011', 'Sebayan', '79460', 'SEEDER'),
+('61.01.01.2012', 'Kartiasa', '79460', 'SEEDER'),
+('61.01.01.2013', 'Saling Rambi', '79460', 'SEEDER'),
+('61.01.01.2014', 'Lumbang', '79460', 'SEEDER'),
+('61.01.01.2015', 'Sungai Rambah', '79460', 'SEEDER'),
+('61.01.01.2023', 'Gapura', '79460', 'SEEDER'),
+('61.01.01.2024', 'Sumber Harapan', '79460', 'SEEDER'),
+('61.01.01.2029', 'Semanggau', '79460', 'SEEDER');
